d462501102c5577bcbceec3b2b97ae0c375c3fb6
Fredy Sanchez <00082817@uca.edu.sv>
2019-06-01 21:27:18 -0600

    Database creada

---
+++ app/src/main/java/com/example/pdm_parcial1/Database/PartidoRoomDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.pdm_parcial1.Database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.pdm_parcial1.Entities.Partido
+import com.example.pdm_parcial1.dao.PartidoDAO
+import kotlinx.coroutines.CoroutineScope
+
+@Database(entities = arrayOf(Partido::class),version = 1)
+public abstract class PartidoRoomDatabase : RoomDatabase() {
+    abstract fun partidoDao() : PartidoDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: PartidoRoomDatabase? = null
+
+        fun getDatabase(context: Context, scope: CoroutineScope): PartidoRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PartidoRoomDatabase::class.java,
+                    "Book_database"
+                ).fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
