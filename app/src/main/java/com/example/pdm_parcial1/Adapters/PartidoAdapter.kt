75b69adf82e9da616a3f681d98c5363abb4b63f3
Fredy Sanchez <00082817@uca.edu.sv>
2019-06-01 23:38:34 -0600

    Adapter de partidos

---
+++ app/src/main/java/com/example/pdm_parcial1/Adapters/PartidoAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.pdm_parcial1.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pdm_parcial1.Entities.Partido
+import com.example.pdm_parcial1.R
+
+class PartidoAdapter internal constructor(context: Context) : RecyclerView.Adapter<PartidoAdapter.PartidoViewHolder>(){
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var partidos = emptyList<Partido>()
+
+    inner class PartidoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val partidoItemView : TextView = itemView.findViewById(R.id.tv_Equipos)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PartidoViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return PartidoViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return partidos.size
+    }
+
+    override fun onBindViewHolder(holder: PartidoViewHolder, position: Int) {
+        val current = partidos[position]
+        holder.partidoItemView.text = current.EquipoA
+    }
+
+    internal fun setPartidos(partidos: List<Partido>){
+        this.partidos = partidos
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/pdm_parcial1/MainActivity.kt
+++ app/src/main/java/com/example/pdm_parcial1/MainActivity.kt
@@ -5,6 +5,9 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pdm_parcial1.Adapters.PartidoAdapter
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,6 +22,12 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = PartidoAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
